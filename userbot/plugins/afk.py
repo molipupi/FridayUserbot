b8fc7f5788b23fefb98e4be14e86a972f558d8e5
molipupi <68815761+molipupi@users.noreply.github.com>
2020-07-27 23:14:56 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -61,9 +61,9 @@ async def _(event):
             afk_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
-            await event.edit(f"My Boss Is Going Afk ! And The Reason is {reason}")
+            await event.edit(f"miss sky Is Going Afk ! And The Reason is {reason}")
         else:
-            await event.edit(f"My Boss is Going")
+            await event.edit(f"miss sky  is Going")
         await asyncio.sleep(5)
         await event.delete()
         try:
@@ -124,7 +124,7 @@ async def on_afk(event):
         message_to_reply = f"**My Boss is AFK** ! \n\n**Reason** : `{reason}` \n\n**Afk Since** : {afk_since}" + \
             f"\n\n__Kindly Leave A Message__ ! \n`He Will Reply To You Soon !`" \
             if reason \
-            else f"**Hello, Boss Is AFK Right Now And May Be Forgot List Reason ! Any Way He Will Come Back Soon !**"
+            else f"**Hello, miss sky AFK Right Now And May Be Forgot List Reason ! Any Way she Will Come Back Soon !**"
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
